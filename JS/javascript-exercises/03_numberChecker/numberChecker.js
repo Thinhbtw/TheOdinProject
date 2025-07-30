5de55651ddd4febb6679a764e217e0ab63030761
Thinhbtw <ducthinhbtw@gmail.com>
2025-07-30 23:47:57 +0700

    Day 7: Data Types and Conditional Do exercise 1-5

---
+++ JS/javascript-exercises/03_numberChecker/numberChecker.js
@@ -0,0 +1,9 @@
+function numberChecker(number) {
+  if(number >= 10) {
+    return true;
+  }
+  return false;
+}
+
+// Do not edit below this line
+module.exports = numberChecker;
---
+++ JS/javascript-exercises/05_joinStrings/joinStrings.js
@@ -0,0 +1,32 @@
+/*
+ First name: Carlos
+ Last name: Stevenson
+ This year: 1965
+ Birth year: 1947
+
+ The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+*/
+
+// Add your code right below, good luck!
+
+const firstName = `Carlos`;
+const lastName = `Stevenson`;
+const thisYear = 1965;
+const birthYear = 1947;
+const age = 18;
+const greeting = `Hello! My name is ${firstName} ${lastName} and I am ${age} years old.`;
+const fullName = `${firstName} ${lastName}`;
+
+
+
+
+// Do not change this
+module.exports = {
+  firstName: typeof firstName === 'undefined' ? undefined : firstName,
+  lastName: typeof lastName === 'undefined' ? undefined : lastName,
+  thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
+  birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
+  greeting: typeof greeting === 'undefined' ? undefined : greeting,
+  fullName: typeof fullName === 'undefined' ? undefined : fullName,
+  age: typeof age === 'undefined' ? undefined : age
+}
